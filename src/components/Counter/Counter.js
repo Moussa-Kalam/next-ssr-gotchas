d649774204fcfeb3252afab73ec77f52c5e7cf00
Moussa Kalam AMZAT <moussakalamamzat@gmail.com>
2025-02-05 11:12:12 +0200

    feat: improve UX with a spinner while getting the saved count value

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,8 +1,9 @@
 'use client';
 import React from 'react';
+import Spinner from '../Spinner'
 
 function Counter() {
-  const [count, setCount] = React.useState(0);
+  const [count, setCount] = React.useState(null);
 
   React.useEffect(() => {
     const savedCount =
@@ -22,7 +23,7 @@ function Counter() {
       className="count-btn"
       onClick={() => setCount(count + 1)}
     >
-      Count: {count}
+      Count: {typeof count === 'number' ? count : <Spinner />}
     </button>
   );
 }
